a2b874c5daea87c580e251563f014a1f0d77c3ae
ZiarayZ <jakesmith@binaryrage.com>
2024-06-27 15:19:52 +0100

    comments

--- src/client/Header.tsx
+++ src/client/Header.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 const Header: React.FC<{}> = _ => {
-    return null;
+    return <h1>Portfolio</h1>;
 }
 
 export default Header;
--- src/client/Portfolio.tsx
+++ src/client/Portfolio.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 
 const Portfolio: React.FC<{}> = _ => {
+    // plan to have/display an array of "cards" of each project I've done
     return null;
 }
 
