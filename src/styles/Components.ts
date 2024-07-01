e8e5280786e5ef36634ebb081d0de28ed7aa0cc6
ZiarayZ <jakesmith@binaryrage.com>
2024-07-01 17:00:27 +0100

    spacings

--- src/styles/Components.ts
+++ src/styles/Components.ts
@@ -2,9 +2,15 @@ import { styled } from "styled-components";
 
 export const StyledCards = styled.div`
     display: flex;
+    gap: 32px;
+    flex-wrap: wrap;
+    align-items: center;
 `;
 
 export const StyledCard = styled.div`
     width: 256px;
     height: 356px;
+
+    padding: 8px;
+    word-wrap: break-word;
 `;
