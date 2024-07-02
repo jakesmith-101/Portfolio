229059c3f1ddf42f65fd64ad79843f7dca732c09
ZiarayZ <jakesmith@binaryrage.com>
2024-07-02 13:24:16 +0100

    base shadow

--- src/styles/Components.ts
+++ src/styles/Components.ts
@@ -10,7 +10,10 @@ export const StyledCards = styled.div`
 export const StyledCard = styled.div`
     width: 256px;
     height: 356px;
+    border-radius: 10px;
 
     padding: 8px;
     word-wrap: break-word;
+
+    box-shadow: 0px 10px 10px 0px rgba(0,0,0,0.5);
 `;
