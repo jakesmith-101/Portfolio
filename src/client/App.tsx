0e221de763f5df2154b38e717155b5f12e19ee1d
ZiarayZ <jakesmith@binaryrage.com>
2024-07-02 15:57:51 +0100

    actually apply background element

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import Portfolio from './Portfolio/Body';
 import Header from './Header';
+import Background from './Background';
 
 const App: React.FC = _ => {
   return <div className="App">
     <Header />
     <div>
+      <Background />
       <Portfolio />
     </div>
   </div>
