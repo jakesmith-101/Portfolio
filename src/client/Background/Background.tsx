923799f9858590430eb15a4e774586c8fe7fb486
ZiarayZ <jakesmith@binaryrage.com>
2024-07-03 15:33:59 +0100

    too much strain on cpu

--- src/client/Background/Background.tsx
+++ src/client/Background/Background.tsx
@@ -1,32 +1,18 @@
 import * as React from 'react';
-import { Lamp, Region, Glass, Bubble, tAnim } from './BGComps';
+import { } from './BGComps';
 import { Chance } from 'chance';
 
-const animTypes: tAnim[] = ["linear", "ease-in", "ease-out", "ease-in-out"];
 const Background: React.FC<React.PropsWithChildren<{}>> = ({ children }) => {
     const C = Chance();
 
-    return <Region>
-        <Lamp>
-            <Glass>
-                { // this is quite intensive to render btw
-                    [...new Array(8)].map((_, id) => <Bubble
-                        key={id}
-                        first={C.pickone(animTypes)}
-                        t1={C.integer({ min: 15, max: 60 })}
-                        second={C.pickone(animTypes)}
-                        t2={C.integer({ min: 15, max: 60 })}
-                        third={C.pickone(animTypes)}
-                        t3={C.integer({ min: 15, max: 60 })}
-                        fourth={C.pickone(animTypes)}
-                        t4={C.integer({ min: 15, max: 60 })}
-                    />)}
-            </Glass>
-        </Lamp>
+    return <div>
+        <div>
+            {/* Background */}
+        </div>
         <div>
             {children}
         </div>
-    </Region>
+    </div>
 }
 
 
