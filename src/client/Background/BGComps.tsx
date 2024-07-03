4e80779343adb09027504c82fd9098860cfde7ba
ZiarayZ <jakesmith@binaryrage.com>
2024-07-03 14:15:00 +0100

    attempt to fix spacing between hyphens by linting

--- src/client/Background/BGComps.tsx
+++ src/client/Background/BGComps.tsx
@@ -1,25 +1,25 @@
 import { styled, keyframes } from 'styled-components';
 
 const updown = keyframes`
-    0 % { top: 110 %; }
-    50 % { top: 0; }
-    100 % { top: 110 %; }
+    0% { top: 110%; }
+    50% { top: 0; }
+    100% { top: 110%; }
 `;
 
 const leftright = keyframes`
-    0 % { left: 0; }
-    50 % { left: 80 %; }
-    100 % { left: 0; }
+    0% { left: 0; }
+    50% { left: 80%; }
+    100% { left: 0; }
 `;
 
 const vastag = keyframes`
-    0 % { width: 8vh; }
-    50 % { width: 32vh; }
-    100 % { width: 8vh; }
+    0% { width: 8vh; }
+    50% { width: 32vh; }
+    100% { width: 8vh; }
 `;
 
 const forog = keyframes`
-    100 % { transform: rotate(360deg); }
+    100% { transform: rotate(360deg); }
 `;
 
 
@@ -31,35 +31,38 @@ export const Lamp = styled.div`
 `;
 
 export const Region = styled.div`
-    background: radial - gradient(#500, #000);
+    background: radial-gradient(#500, #000);
     margin: 0;
 `;
 
 
 export const Bubble = styled.div`
     height: 16vh;
-    border - radius: 40 %;
+    border-radius: 40%;
     background: red;
     position: absolute;
     filter: blur(4vh);
-    animation: ${updown} linear 37s infinite,
-        ${leftright} ease -in -out 23s infinite,
-            ${vastag} linear 29s infinite,
-                ${forog} linear 31s infinite;
 `;
 
 
 export const Glass = styled.div`
     overflow: hidden;
-    width: 100 %;
+    width: 100%;
     height: 90vh;
     position: relative;
     top: -4vh;
     transform: perspective(100vh) rotateX(12deg);
     filter: contrast(22);
-    background: linear - gradient(blue 30 %, #60a 80 %, #a06 100 %);
-    box - shadow: inset 0 - 5vh 1vh 1vh rgba(255, 0, 0, .6), 0 0 2.5px .2px #808;
-    border - radius: 1vh 1vh 5vh 5vh / 20 %;
+    background: linear-gradient(blue 30%, #60a 80%, #a06 100%);
+    box-shadow: inset 0-5vh 1vh 1vh rgba(255, 0, 0, .6), 0 0 2.5px .2px #808;
+    border-radius: 1vh 1vh 5vh 5vh/20%;
+
+    ${Bubble}:first-child {
+        animation: ${updown} linear 37s infinite,
+            ${leftright} ease-in-out 23s infinite,
+                ${vastag} linear 29s infinite,
+                    ${forog} linear 31s infinite;
+    }
 
     ${Bubble}:nth-child(2) {
         animation: ${updown} ease-in 41s infinite,
