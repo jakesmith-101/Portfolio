88ef4b5d25e7a9587e247363e8ebf2400669baec
ZiarayZ <jakesmith@binaryrage.com>
2024-07-06 15:05:28 +0100

    remove size attempts

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -3,19 +3,9 @@ import Portfolio from './Portfolio/Body';
 import Background from './Background/Background';
 
 const App: React.FC = _ => {
-  const bounds = React.useMemo(() => {
-    const app = document.getElementsByClassName("App").item(0);
-    if (app !== null) {
-      return {
-        width: app.clientWidth,
-        height: app.clientHeight
-      }
-    }
-    return null;
-  }, [window.innerWidth, window.innerHeight, window.outerHeight, window.outerWidth]);
 
   return <div className="App">
-    <Background bounds={bounds}>
+    <Background>
       <Portfolio />
     </Background>
   </div>
--- src/client/Background/Background.tsx
+++ src/client/Background/Background.tsx
@@ -9,20 +9,16 @@ const random = (max: number, min?: number) => {
         return max + Math.floor(Math.random() * (min - max + 1)); // swap max/min
     else
         return min;
-}
+};
+const StarBuilder = (n: number) => {
+    let value = `${random(2000)}px ${random(2000)}px #FFF`;
+    for (let i = 1; i < n; i++) {
+        value = `${value} , ${random(2000)}px ${random(2000)}px #FFF`;
+    }
+    return value;
+};
 
-const Background: React.FC<React.PropsWithChildren<{ bounds: { width: number, height: number } | null }>> = ({ bounds, children }) => {
-    const StarBuilder = React.useCallback((n: number) => {
-        const width = bounds?.width ?? 2000;
-        const height = bounds?.height ?? 2000;
-
-        let value = `${random(100) * width / 100}px ${random(100) * height / 100}px #FFF`;
-        for (let i = 1; i < n; i++) {
-            value = `${value} , ${random(100) * width / 100}px ${random(100) * height / 100}px #FFF`;
-        }
-        return value;
-    }, [bounds?.width, bounds?.height]);
-
+const Background: React.FC<React.PropsWithChildren<{}>> = ({ children }) => {
     return <BGColour>
         <StarCurtain stars={StarBuilder(700)} size={1} />
         <StarCurtain stars={StarBuilder(200)} size={2} />
