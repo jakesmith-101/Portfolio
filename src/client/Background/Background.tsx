b2d019a1eabb3ee8f5ef2f2138c905ccd5070ab4
ZiarayZ <jakesmith@binaryrage.com>
2024-07-06 14:49:24 +0100

    testing element size

--- src/client/Background/Background.tsx
+++ src/client/Background/Background.tsx
@@ -10,20 +10,25 @@ const random = (max: number, min?: number) => {
     else
         return min;
 }
-const StarBuilder = (n: number) => {
-    let value = `${random(100)}% ${random(100)}% #FFF`;
-    for (let i = 2; i < n; i++) {
-        value = `${value} , ${random(100)}% ${random(100)}% #FFF`;
-    }
-    return value;
-}
 
 const Background: React.FC<React.PropsWithChildren<{}>> = ({ children }) => {
+    const StarBuilder = React.useCallback((n: number) => {
+        const container = document.getElementById("children");
+        const width = container?.clientWidth ?? 2000;
+        const height = container?.clientHeight ?? 2000;
+
+        let value = `${random(100) * width / 100}px ${random(100) * height / 100}px #FFF`;
+        for (let i = 2; i < n; i++) {
+            value = `${value} , ${random(100) * width / 100}px ${random(100) * height / 100}px #FFF`;
+        }
+        return value;
+    }, [document.getElementById]);
+
     return <BGColour>
         <StarCurtain stars={StarBuilder(700)} size={1} />
         <StarCurtain stars={StarBuilder(200)} size={2} />
         <StarCurtain stars={StarBuilder(100)} size={3} />
-        <div>
+        <div id="children">
             {children}
         </div>
     </BGColour>
