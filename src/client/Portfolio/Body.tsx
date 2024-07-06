e92591ab825726657bc4ad908cc7289bb1b75282
ZiarayZ <jakesmith@binaryrage.com>
2024-07-06 15:13:21 +0100

    allow viewing any user's repos

--- src/client/Portfolio/Body.tsx
+++ src/client/Portfolio/Body.tsx
@@ -6,39 +6,31 @@ import Cards from './Cards';
 const work = "jakesmith-101"; // `https://api.github.com/users/${work}/repos`
 const games = "ZiarayZ"; //      `https://api.github.com/users/${games}/repos`
 
-const Portfolio: React.FC<{}> = _ => {
-    const [workRepos, setWork] = React.useState<RepoType | null>(null);
-    const [gameRepos, setGames] = React.useState<RepoType | null>(null);
-    const [error, setError] = React.useState<any | null>(null);
+const Portfolio: React.FC<{ users: string[] }> = ({ users }) => {
+    const [repos, setRepos] = React.useState<RepoType | null>(null);
+    const [error, setError] = React.useState<any[] | null>(null);
 
     React.useEffect(() => {
-        fetch(`https://api.github.com/users/${work}/repos`)
-            .then(response => response.json())
-            .then(setWork)
-            .catch(setError);
-        fetch(`https://api.github.com/users/${games}/repos`)
-            .then(response => response.json())
-            .then(setGames)
-            .catch(setError);
-    }, [work]);
-
-    const repos = React.useMemo((): RepoType | null => {
-        if (workRepos !== null && gameRepos !== null)
-            return [...workRepos, ...gameRepos];
-        else if (workRepos !== null)
-            return workRepos;
-        else if (gameRepos !== null)
-            return gameRepos;
-        else
-            return null;
-    }, [workRepos, gameRepos]);
+        users.forEach(user => {
+            fetch(`https://api.github.com/users/${user}/repos`)
+                .then(response => response.json())
+                .then(setRepos)
+                .catch(e => {
+                    setError(prev => {
+                        if (prev === null) return [e];
+                        else prev.push(e);
+                        return prev;
+                    })
+                });
+        });
+    }, [users.join(",")]);
 
     return <div>
         {error === null ? <PlayArea>
             {repos !== null ? <Cards repos={repos} /> : 'Loading...'}
         </PlayArea> : <div>
             <h3>Error:</h3>
-            {error}
+            {error.map((e, id) => <div key={id}>{e}</div>)}
         </div>}
     </div>
 }
