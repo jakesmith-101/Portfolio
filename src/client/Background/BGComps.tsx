5e7f5d52ed1b77772bf74e2b1ece7fc0b114d909
ZiarayZ <jakesmith@binaryrage.com>
2024-07-06 14:12:39 +0100

    background update test

--- src/client/Background/BGComps.tsx
+++ src/client/Background/BGComps.tsx
@@ -1,2 +1,73 @@
 import { styled, keyframes } from 'styled-components';
 
+export const BGColour = styled.div`
+    overflow: hidden;
+    background-color: radial-gradient(ellipse, #1B2735 0%, #090A0F 100%);
+`;
+
+export const Container = styled.div`
+    overflow: scroll;
+`;
+
+
+const animStar = keyframes`
+    0% {
+        transform: translateY(0px);
+    }
+    100% {		
+        transform: translateY(-2000px);
+    }
+`;
+
+export const FirstStarCurtain = styled.div<{ stars: string }>`
+    width: 1px;
+    height: 1px;
+    background: transparent;
+    box-shadow: ${p => p.stars};
+    animation: ${animStar} 50s linear infinite;
+        
+    &:after {
+        content: " ";
+        position: absolute;
+        top: 2000px;
+        width: 1px;
+        height: 1px;
+        background: transparent;
+        box-shadow: ${p => p.stars};
+    }
+`;
+
+export const SecondStarCurtain = styled.div<{ stars: string }>`
+    width: 2px;
+    height: 2px;
+    background: transparent;
+    box-shadow: ${p => p.stars};
+    animation: ${animStar} 100s linear infinite;
+        
+    &:after {
+        content: " ";
+        position: absolute;
+        top: 2000px;
+        width: 2px;
+        height: 2px;
+        background: transparent;
+        box-shadow: ${p => p.stars};
+    }
+`;
+
+export const ThirdStartCurtain = styled.div<{ stars: string }>`
+    width: 3px;
+    height: 3px;
+    background: transparent;
+    box-shadow: ${p => p.stars};
+    animation: ${animStar} 150s linear infinite;
+        
+    &:after
+        content: " ";
+        position: absolute;
+        top: 2000px;
+        width: 3px;
+        height: 3px;
+        background: transparent;
+        box-shadow: ${p => p.stars};
+`;
--- src/client/Background/Background.tsx
+++ src/client/Background/Background.tsx
@@ -1,18 +1,35 @@
 import * as React from 'react';
-import { } from './BGComps';
+import { BGColour, Container, FirstStarCurtain, SecondStarCurtain, ThirdStartCurtain } from './BGComps';
 import { Chance } from 'chance';
+
+const random = (max: number, min?: number) => {
+    min = min ?? 0;
+    if (min < max)
+        return min + Math.floor(Math.random() * (max - min + 1));
+    else if (max < min)
+        return max + Math.floor(Math.random() * (min - max + 1)); // swap max/min
+    else
+        return min;
+}
+const StarBuilder = (n: number) => {
+    let value = `${random(100)}% ${random(100)}% #FFF`;
+    for (let i = 2; i < n; i++) {
+        value = `${value} , ${random(100)}% ${random(100)}% #FFF`;
+    }
+    return value;
+}
 
 const Background: React.FC<React.PropsWithChildren<{}>> = ({ children }) => {
     const C = Chance();
 
-    return <div>
-        <div>
-            {/* Background */}
-        </div>
-        <div>
+    return <BGColour>
+        <FirstStarCurtain stars={StarBuilder(700)} />
+        <SecondStarCurtain stars={StarBuilder(200)} />
+        <ThirdStartCurtain stars={StarBuilder(100)} />
+        <Container>
             {children}
-        </div>
-    </div>
+        </Container>
+    </BGColour>
 }
 
 
