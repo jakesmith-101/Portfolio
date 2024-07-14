308e887cde4263fb974cb634243e00b77ad73e19
ZiarayZ <jakesmith@binaryrage.com>
2024-07-14 15:18:59 +0100

    linkify name

--- src/client/Portfolio/Card.tsx
+++ src/client/Portfolio/Card.tsx
@@ -3,10 +3,10 @@ import { StyledCard } from '../../styles/Components';
 import RepoType from '../../types/Repository';
 
 const Card: React.FC<RepoType[0]> = React.memo(props => {
-    const { name, description, owner } = props;
+    const { name, description, owner, html_url } = props;
     return <StyledCard>
         <h5><a href={owner.html_url}>{typeof owner.name === "string" ? `@${owner.name}` : owner.login}</a></h5>
-        <h6>{name}</h6>
+        <h6><a href={html_url}>{name}</a></h6>
         <p>{description}</p>
     </StyledCard>;
 })
