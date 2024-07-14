a165347a0ecf59c67a92d9b0361896b7e9dc6581
ZiarayZ <jakesmith@binaryrage.com>
2024-07-14 15:17:26 +0100

    fix user selection

--- src/client/Portfolio/PlayArea.tsx
+++ src/client/Portfolio/PlayArea.tsx
@@ -9,21 +9,30 @@ const Portfolio: React.FC<{ users: string[] }> = ({ users }) => {
 
     React.useEffect(() => {
         (async () => {
-            for (const user in users) {
-                try {
-                    const response = await fetch(`https://api.github.com/users/${user}/repos`);
-                    const json: RepoType = await response.json(); // making assumptions on Type here, but should be ok for this use case...
-                    setRepos(prev => {
-                        if (prev === null) return json;
-                        else prev.push(...json);
-                        return prev;
-                    });
-                } catch (e) {
+            for (let i = 0; i < users.length; i++) {
+                const user = users.at(i);
+                if (user !== undefined) {
+                    try {
+                        const response = await fetch(`https://api.github.com/users/${user}/repos`);
+                        const json: RepoType = await response.json(); // making assumptions on Type here, but should be ok for this use case...
+                        setRepos(prev => {
+                            if (prev === null) return json;
+                            else prev.push(...json);
+                            return prev;
+                        });
+                    } catch (e) {
+                        setError(prev => {
+                            if (prev === null) return [e];
+                            else prev.push(e);
+                            return prev;
+                        });
+                    }
+                } else {
                     setError(prev => {
-                        if (prev === null) return [e];
-                        else prev.push(e);
+                        if (prev === null) return [new RangeError("Index out of range")];
+                        else prev.push(new RangeError("Index out of range"));
                         return prev;
-                    });
+                    })
                 }
             }
         })();
