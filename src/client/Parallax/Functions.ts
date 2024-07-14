28060eabe4bbed5ddbd671b2c8841f82a93624fd
ZiarayZ <jakesmith@binaryrage.com>
2024-07-14 16:05:46 +0100

    testing parallax feature (experimental)

---
+++ src/client/Parallax/Functions.ts
@@ -0,0 +1,2 @@
+const range = 40;
+export const calcValue = (a: number, b: number) => (a / b * range - range / 2).toFixed(1)
--- src/client/Portfolio/Card.tsx
+++ src/client/Portfolio/Card.tsx
@@ -1,13 +1,52 @@
 import * as React from 'react';
 import { StyledCard } from '../../styles/Components';
 import RepoType from '../../types/Repository';
+import { calcValue } from '../Parallax/Functions';
 
 const Card: React.FC<RepoType[0]> = React.memo(props => {
     const { name, description, owner, html_url } = props;
-    return <StyledCard>
-        <h5><a href={owner.html_url}>{typeof owner.name === "string" ? `@${owner.name}` : owner.login}</a></h5>
-        <h6><a href={html_url}>{name}</a></h6>
-        <p>{description}</p>
+    const [timeout, setTimeout] = React.useState<number>();
+
+    const handleMouseMove = React.useCallback(({ x, y }: { x: number, y: number }) => {
+        if (timeout) {
+            window.cancelAnimationFrame(timeout);
+        }
+
+        setTimeout(window.requestAnimationFrame(() => {
+            const yValue = calcValue(y, window.innerHeight);
+            const xValue = calcValue(x, window.innerWidth);
+
+            const card = document.getElementById("card");
+            if (card !== null) {
+                card.style.transform = `rotateX(${yValue}deg) rotateY(${xValue}deg)`;
+
+                const texts = Array.from(card.children);
+                [].forEach.call(texts, (text: Element) => {
+                    if (text.tagName.toUpperCase() === "DIV")
+                        (text as HTMLDivElement).style.transform = `translateX(${-xValue}px) translateY(${yValue}px)`;
+                });
+            }
+        }));
+    }, [timeout, setTimeout]);
+
+    React.useEffect(() => {
+        const card = document.getElementById("card");
+        if (card !== null)
+            card.addEventListener('mousemove', handleMouseMove, false);
+
+        // cleanup this component
+        return () => {
+            if (card !== null)
+                card.removeEventListener('mousemove', handleMouseMove, false);
+        };
+    }, []);
+
+    return <StyledCard id="card">
+        <div>
+            <h5><a href={owner.html_url}>{typeof owner.name === "string" ? `@${owner.name}` : owner.login}</a></h5>
+            <h6><a href={html_url}>{name}</a></h6>
+            <p>{description}</p>
+        </div>
     </StyledCard>;
 })
 
