df8c08c16696602fd6ece4417bd73dac22d12df1
ZiarayZ <jakesmith@binaryrage.com>
2024-07-14 16:54:16 +0100

    undo dedupe

--- src/client/Portfolio/Card.tsx
+++ src/client/Portfolio/Card.tsx
@@ -7,13 +7,17 @@ const Card: React.FC<RepoType[0]> = React.memo(props => {
     const { name, description, owner, html_url, id } = props;
     const [timeout, setTimeout] = React.useState<number>();
 
-    const handleAnimation = React.useCallback((xValue: string, yValue: string) => {
+    // hover animation
+    const handleMouseMove = React.useCallback(({ x, y }: { x: number, y: number }) => {
         if (timeout)
             window.cancelAnimationFrame(timeout);
 
         setTimeout(window.requestAnimationFrame(() => {
             const card = document.getElementById(`${id}`);
             if (card !== null) {
+                const { top, left } = card.getBoundingClientRect();
+                const yValue = calcValue(y - top, card.clientHeight);
+                const xValue = calcValue(x - left, card.clientWidth);
                 card.style.transform = `rotateX(${yValue}deg) rotateY(${-xValue}deg)`;
 
                 /* Parallax for children, probably for images?:
@@ -27,19 +31,26 @@ const Card: React.FC<RepoType[0]> = React.memo(props => {
         }));
     }, [id, timeout, setTimeout]);
 
-    // hover animation
-    const handleMouseMove = React.useCallback(({ x, y }: { x: number, y: number }) => {
-        const card = document.getElementById(`${id}`);
-        if (card !== null) {
-            const { top, left } = card.getBoundingClientRect();
-            const yValue = calcValue(y - top, card.clientHeight);
-            const xValue = calcValue(x - left, card.clientWidth);
-            handleAnimation(xValue, yValue);
-        }
-    }, [id, handleAnimation]);
-
     // resetter
-    const handleMouseLeave = React.useCallback(() => handleAnimation("0", "0"), [handleAnimation]);
+    const handleMouseLeave = React.useCallback(() => {
+        if (timeout)
+            window.cancelAnimationFrame(timeout);
+
+        setTimeout(window.requestAnimationFrame(() => {
+            const card = document.getElementById(`${id}`);
+            if (card !== null) {
+                card.style.transform = `rotateX(0deg) rotateY(0deg)`;
+
+                /* Parallax for children, probably for images?:
+                const texts = Array.from(card.children);
+                [].forEach.call(texts, (text: Element) => {
+                    if (text.tagName.toUpperCase() === "DIV")
+                        (text as HTMLDivElement).style.transform = `translateX(0px) translateY(0px)`;
+                });
+                */
+            }
+        }));
+    }, [id, timeout, setTimeout]);
 
     React.useEffect(() => {
         const card = document.getElementById(`${id}`);
